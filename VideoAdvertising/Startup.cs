a98ab0eb36eeaa7296a12685219838bca890dd96
jmsaucier <jmsaucier91@gmail.com>
2016-11-28 21:43:12 -0600

    initial commit

---
+++ VideoAdvertising/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoAdvertising.Startup))]
+namespace VideoAdvertising
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
