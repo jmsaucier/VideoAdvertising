54c92d29c9769e79c08c164327a72610770a7e29
jmsaucier <jmsaucier91@gmail.com>
2016-12-30 21:41:35 -0600

    Started DataAccesor interfaces, and stubbed first implementation.

---
+++ VideoAdvertising.Common/DataAccessorInterfaces/IUserDataAccessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoAdvertising.Common.Objects;
+
+namespace VideoAdvertising.Common.DataAccessInterfaces
+{
+    public interface IUserDataAccessor
+    {
+        User GetUser(string id);
+
+        List<User> GetUsers();
+
+        User StoreUser(User user);
+
+        User UpdateUser(User user);
+    }
+}
---
+++ VideoAdvertising.DataAccessLayer/DataAccessorImplementations/UserDataAccessorAccessorImplementation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoAdvertising.Common.DataAccessInterfaces;
+using VideoAdvertising.Common.Objects;
+
+namespace VideoAdvertising.DataAccessLayer.DataAccessorObjects
+{
+    public class UserDataAccessorImplementation : IUserDataAccessor
+    {
+        public User GetUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User StoreUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
