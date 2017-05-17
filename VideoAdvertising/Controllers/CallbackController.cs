0efdcadca99152c6dc4af589ebcb050f04022490
Mason <jmsaucier91@gmail.com>
2017-05-16 20:02:29 -0500

    Callback added

---
+++ VideoAdvertising/Controllers/CallbackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoAdvertising.Controllers
+{
+    public class CallbackController : Controller
+    {
+        // GET: Callback
+        public ActionResult Index(string code, string state)
+        {
+            return View();
+        }
+    }
+}
