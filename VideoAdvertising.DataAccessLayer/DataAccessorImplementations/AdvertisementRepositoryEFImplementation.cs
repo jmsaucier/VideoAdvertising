88b028e15251af89e3a29aa4699de0ec08f268e1
jmsaucier <jmsaucier91@gmail.com>
2017-05-18 21:42:41 -0500

    Start of advertisement repository tests.

---
+++ VideoAdvertising.DataAccessLayer/DataAccessorImplementations/AdvertisementRepositoryEFImplementation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoAdvertising.Common.Interfaces.DataAccessInterfaces;
+using VideoAdvertising.Common.Interfaces.ObjectInterfaces;
+using VideoAdvertising.Common.Objects.ModelObjects;
+
+namespace VideoAdvertising.DataAccessLayer.DataAccessorImplementations
+{
+    public class AdvertisementRepositoryEFImplementation : IAdvertisementRepository
+    {
+        public IAdvertisement GetById(string id)
+        {
+            return new Advertisement();
+        }
+
+        public IEnumerable<IAdvertisement> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAdvertisement Store(IAdvertisement value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAdvertisement Update(string id, IAdvertisement value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- VideoAdvertising.Tests/DataAccessLayer/DataAccessorImplementations/AdvertisementRepositoryEFImplementationTest.cs
+++ VideoAdvertising.Tests/DataAccessLayer/DataAccessorImplementations/AdvertisementRepositoryEFImplementationTest.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using VideoAdvertising.DataAccessLayer.DataAccessorImplementations;
 
 namespace VideoAdvertising.Tests.DataAccessLayer.DataAccessorImplementations
 {
-    class AdvertisementRepositoryEFImplementationTest
+    [TestFixture]
+    public class AdvertisementRepositoryEFImplementationTest
     {
+        [TestFixture]
+        public class Constructor
+        {
+            [Test]
+            public void Is_Not_Null()
+            {
+                Assert.IsNotNull(new AdvertisementRepositoryEFImplementation());
+            }
+        }
+
+        [TestFixture]
+        public class GetById
+        {
+            [Test]
+            public void Is_Not_Null()
+            {
+                Assert.IsNotNull(new AdvertisementRepositoryEFImplementation().GetById("1"));
+            }
+
+            [Test]
+            public void Returns_Expected_Value()
+            {
+                
+            }
+        }
+        
     }
 }
