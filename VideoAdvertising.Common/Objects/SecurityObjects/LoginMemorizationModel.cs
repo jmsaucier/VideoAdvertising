8a4fc0a3a162465f6a7cfaa707f7e2c180e46862
Mason <jmsaucier91@gmail.com>
2017-06-05 22:15:22 -0500

    Removed unused usings.

--- VideoAdvertising.Common/Objects/SecurityObjects/LoginMemorizationModel.cs
+++ VideoAdvertising.Common/Objects/SecurityObjects/LoginMemorizationModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VideoAdvertising.Common.Objects.SecurityObjects
+﻿namespace VideoAdvertising.Common.Objects.SecurityObjects
 {
     public class LoginMemorizationModel
     {
